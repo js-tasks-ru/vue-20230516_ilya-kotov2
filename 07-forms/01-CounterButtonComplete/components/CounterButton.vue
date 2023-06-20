95167a3105e0519bcf5948992103d75db8517776
Ilya Kotof <zdobnovi@yandex.ru>
2023-06-20 21:51:28 +0300

    error in solving the problem

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,37 @@
 <template>
-  <button></button>
+  <button @click="addCounter">{{ localCounter }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCounter: this.count,
+    };
+  },
+
+  methods: {
+    addCounter() {
+      this.localCounter += 1;
+      this.$emit('update:count', this.localCounter);
+    },
+  },
+
+  watch: {
+    count() {
+      this.localCounter = this.count;
+    },
+  },
 };
 </script>
